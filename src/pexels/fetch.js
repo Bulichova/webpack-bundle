771b7e21ce5141c0513d8f0ff0a86959023685db
Bulichova <bulichova84@gmail.com>
2022-01-13 00:39:08 +0200

    perPage

--- src/pexels/fetch.js
+++ src/pexels/fetch.js
@@ -1,50 +1,83 @@
 import refs from './refs.js'
-const { pexelsForm, galleryId } = refs
-function getFetch(search) {
+const { pexelsForm, galleryId, hiddenButton } = refs
+function getFetch() {
   const pexelsKey = '563492ad6f917000010000019b12c0ae48b44b77908448361ebdac71'
   const baseUrl = `https://api.pexels.com/v1/`
   let endPoint = 'search'
-  let color = 'red'
-  //   let search = 'dog'
-  const params = `?query=${search}&per_page=3&color=${color}`
-  let url = baseUrl + endPoint + params
+  let page = 1
+  let search = ''
+  let color = ''
+  function setPage() {
+    page += 1
+    console.log(page)
+    return page
+  }
+  function setSearch(value) {
+    return (search = value)
+  }
+  function setColor(value) {
+    return (color = value)
+  }
   const option = {
     method: 'GET',
     headers: {
       Authorization: pexelsKey,
     },
   }
-  fetch(url, option)
-    .then((response) => {
-      console.log(response)
-      return response.json()
-    })
-    .then((result) => {
-      console.log(result)
-      return result.photos
-    })
-    .then((data) => {
-      console.log(data)
-      const markup = data
-        .map((elem) => {
-          console.log(elem)
-          const {
-            alt,
-            src: { tiny },
-          } = elem
-          return createElement(tiny, alt)
-        })
-        .join('')
-      console.log(markup)
-      innerElement(galleryId, markup)
-    })
+  let params = `?query=${search}&per_page=2&color=${color}&page=${page}`
+  let url = baseUrl + endPoint + params
+  function getResult() {
+    params = `?query=${search}&per_page=2&color=${color}&page=${page}`
+    url = baseUrl + endPoint + params
+    console.log('Linked Result', url)
+    return fetch(url, option)
+      .then((response) => {
+        // console.log(response)
+        return response.json()
+      })
+      .then((result) => {
+        console.log(result)
+        let condition =
+          Math.ceil(result.total_results / result.per_page) === result.page
+        if (condition) {
+          hiddenButton.classList.add('hiddenButton')
+        } else {
+          hiddenButton.classList.remove('hiddenButton')
+        }
+        // console.log(condition)
+        return result.photos
+      })
+      .then((data) => {
+        // console.log(data)
+        const markup = data
+          .map((elem) => {
+            // console.log(elem)
+            const {
+              alt,
+              src: { tiny },
+            } = elem
+            return createElement(tiny, alt)
+          })
+          .join('')
+        // console.log(markup)
+        innerElement(galleryId, markup)
+      })
+  }
+  return { setPage, getResult, setSearch, setColor }
 }
-console.log(pexelsForm)
+const x = getFetch()
+// console.log(x)
 pexelsForm.addEventListener('submit', (event) => {
   event.preventDefault()
+  galleryId.innerHTML = ''
   let search = event.target.elements.search.value.toLowerCase()
-  console.log(search)
-  getFetch(search)
+  x.setSearch(search)
+  let color = event.target.elements.color.value
+  x.setColor(color)
+  // console.log(color)
+  // console.log(search)
+  x.getResult()
+  event.target.reset()
 })
 //функция создание элемента
 
@@ -61,3 +94,8 @@ function innerElement(place, element) {
     place.insertAdjacentElement('afterbegin', element)
   }
 }
+hiddenButton.addEventListener('click', (event) => {
+  // console.log('click')
+  x.setPage()
+  x.getResult()
+})
